770c21fc9fa8c2dd69b78539c4c3815ec46dd129
dev22 <ysoli36512@gmail.com>
2024-01-23 22:16:04 +0900

    springboot에 jsp 화면 연결하기

--- src/main/java/com/choi/foodie/controller/UserController.java
+++ src/main/java/com/choi/foodie/controller/UserController.java
@@ -5,22 +5,30 @@ import com.choi.foodie.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
-@RequestMapping(value = "/api/v1/app/")
+@Controller
+@RequestMapping(value = "/")
 public class UserController {
 
     @Autowired
     UserService userService;
 
-    @RequestMapping(value = "findAll", method = RequestMethod.POST)
+    @RequestMapping(value = "api/v1/app/findAll", method = RequestMethod.POST)
     public ResponseEntity<?> findAll(){
         ResponseDTO responseDTO = new ResponseDTO();
         responseDTO.setResultCode("S0001");
         responseDTO.setRes(userService.findAll());
         return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/")	public ModelAndView index() throws Exception {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("index"); //jsp(html)로 갈때는 setViewName // class로 갈때는 setView
+
+        return mav;
     }
 }
